e1e441127fc138b7af70c9e774b06e05b00f0754
Gabriel <gabrielyepez04@gmail.com>
2025-10-07 16:33:31 -0400

    integration of footer section

--- mi_portafolio/src/App.tsx
+++ mi_portafolio/src/App.tsx
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero";
 import { Technologies } from "./components/Technologies";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </>
   )
 }
