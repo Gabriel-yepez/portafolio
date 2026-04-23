d0dfa56aa5d0210b535bb1bef109df2441cc7026
Gabriel Yepez <125819572+Gabriel-yepez@users.noreply.github.com>
2026-04-23 05:36:54 -0400

    feat(frontend): wrap app with QueryClientProvider

--- mi_portafolio/src/App.tsx
+++ mi_portafolio/src/App.tsx
@@ -1,25 +1,26 @@
-import { Header } from "./components/Header";
-import { About } from "./components/About"
-import { Hero } from "./components/Hero";
-import { Technologies } from "./components/Technologies";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
+import { Header } from './components/Header'
+import { About } from './components/About'
+import { Hero } from './components/Hero'
+import { Technologies } from './components/Technologies'
+import { Projects } from './components/Projects'
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
 
 function App() {
-
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <main>
-        <Hero/>
+        <Hero />
         <About />
         <Technologies />
         <Projects />
         <Contact />
       </main>
       <Footer />
-    </>
+    </QueryClientProvider>
   )
 }
 
